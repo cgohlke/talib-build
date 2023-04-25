dca97e147a7219fa445be2cf53b32820c493c598
Christoph Gohlke <cgohlke@cgohlke.com>
2023-04-24 17:48:37 -0700

    v0.4.26

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # talib-build
-Build TA-Lib wheels for Windows
+Build [TA-Lib](https://github.com/ta-lib/ta-lib-python) wheels for Python on Windows.
---
+++ build.cmd
@@ -0,0 +1,24 @@
+:: Download and build TA-Lib source code
+@echo on
+
+curl -L -o talib.zip https://sourceforge.net/projects/ta-lib/files/ta-lib/%TALIB_C_VER%/ta-lib-%TALIB_C_VER%-msvc.zip
+if errorlevel 1 exit /B 1
+
+tar -xf talib.zip
+if errorlevel 1 exit /B 1
+
+del talib\_ta_lib.c
+
+curl -L -o talib-python.zip https://github.com/TA-Lib/ta-lib-python/archive/refs/tags/TA_Lib-%TALIB_PY_VER%.zip
+if errorlevel 1 exit /B 1
+
+tar -xf talib-python.zip --strip-components=1
+if errorlevel 1 exit /B 1
+
+git apply --verbose --binary talib.diff
+if errorlevel 1 exit /B 1
+
+:: set MSBUILDTREATHIGHERTOOLSVERSIONASCURRENT
+
+msbuild ta-lib\c\ide\vs2022\lib_proj\ta_lib.sln /m /t:Clean;Rebuild /p:Configuration=cdr /p:Platform=%VS_PLATFORM%
+if errorlevel 1 exit /B 1
