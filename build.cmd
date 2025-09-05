1115c2ba485a27d18f90ff0a09bac68baac2a5c4
Christoph Gohlke <cgohlke@cgohlke.com>
2025-09-04 18:27:57 -0700

    v0.6.7

--- build.cmd
+++ build.cmd
@@ -2,7 +2,7 @@
 @echo on
 
 :: set TALIB_C_VER=0.6.4
-:: set TALIB_PY_VER=0.6.5
+:: set TALIB_PY_VER=0.6.7
 
 set CMAKE_GENERATOR=NMake Makefiles
 set CMAKE_BUILD_TYPE=Release
@@ -11,13 +11,13 @@ set CMAKE_CONFIGURATION_TYPES=Release
 curl -L -o talib-c.zip https://github.com/TA-Lib/ta-lib/archive/refs/tags/v%TALIB_C_VER%.zip
 if errorlevel 1 exit /B 1
 
-curl -L -o talib-python.zip https://github.com/TA-Lib/ta-lib-python/archive/refs/tags/TA_Lib-%TALIB_PY_VER%.zip
+curl -L -o talib-python.tar.gz https://github.com/TA-Lib/ta-lib-python/releases/download/v%TALIB_PY_VER%/ta_lib-%TALIB_PY_VER%.tar.gz
 if errorlevel 1 exit /B 1
 
 tar -xzvf talib-c.zip
 if errorlevel 1 exit /B 1
 
-tar -xf talib-python.zip --strip-components=1
+tar -xzvf talib-python.tar.gz --strip-components=1
 if errorlevel 1 exit /B 1
 
 :: git apply --verbose --binary talib.diff
